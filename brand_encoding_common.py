dc92a642e72a303a836160cf4ccce49ad27130f3
duongtungnguyen1603 <tungduongnguyen153@gmail.com>
2025-03-25 01:20:14 +0700

    brand_encoding_common.py

--- brand_encoding_common.py
+++ brand_encoding_common.py
@@ -3,7 +3,7 @@ from sklearn.preprocessing import LabelEncoder
 import joblib
 
 # Đọc dữ liệu
-df = pd.read_csv("E:/Du_an_ca_nhan/Cleaned_Sneakers_dataset.csv")
+df = pd.read_csv("Cleaned_Sneakers_dataset.csv")
 
 # Xử lý NaN trong Brand bằng giá trị xuất hiện nhiều nhất
 most_common_brand = df["Brand"].mode()[0]  
@@ -14,7 +14,7 @@ label_encoder = LabelEncoder()
 df["Brand"] = label_encoder.fit_transform(df["Brand"])
 
 # Lưu LabelEncoder & giá trị mode của Brand
-joblib.dump(label_encoder, "E:/Du_an_ca_nhan/brand_encoder.pkl")
-joblib.dump(most_common_brand, "E:/Du_an_ca_nhan/most_common_brand.pkl")
+joblib.dump(label_encoder, "brand_encoder.pkl")
+joblib.dump(most_common_brand, "most_common_brand.pkl")
 
 print(f"Đã lưu brand_encoder.pkl và most_common_brand.pkl (Mode: {most_common_brand})")
